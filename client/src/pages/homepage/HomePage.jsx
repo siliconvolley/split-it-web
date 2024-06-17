e8e5acdfa7c1371c388637c8f10b0574e2f51f36
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-06-17 19:54:37 +0530

    fixed responsiveness for large screens

--- client/src/pages/homepage/HomePage.jsx
+++ client/src/pages/homepage/HomePage.jsx
@@ -5,30 +5,32 @@ import HomeIllustration from '../../assets/home_illustration.svg';
 function HomePage() {
   return (
     <>
-      <div className="mx-5 flex flex-col justify-end align-middle h-dvh lg:mx-[40rem]">
-        <div>
-          <img
-            src={HomeIllustration}
-            alt="Home Illustration"
-            className="max-w-[35rem] h-auto mx-auto"
-          />
-        </div>
-        <div className="home-title-container">
-          <div className="text-[1.5rem] italic">Welcome to</div>
-          <div className="uppercase text-[4rem] font-extrabold tracking-wide mt-[-0.5rem]">
-            Split.it
+      <div className="flex justify-center items-center min-h-screen ">
+        <div className="mx-5 flex flex-col justify-end align-middle h-dvh max-w-[40rem] w-full">
+          <div>
+            <img
+              src={HomeIllustration}
+              alt="Home Illustration"
+              className="max-w-[35rem] h-auto mx-auto"
+            />
           </div>
-          <span className="text-[0.9rem] text-justify lg:text-[1.25rem]">
-            "<span className="underline font-semibold">Seamlessly</span> divide
-            your bills according to individual choices or equally, guaranteeing
-            everyone pays their fair share with ease."
-          </span>
-        </div>
-        <div className="flex justify-center my-[6rem]">
-          <div className="w-[15rem] text-center bg-neutral-800 hover:bg-neutral-700 text-white font-bold py-3 px-4 rounded-lg">
-            <Link to="/bill" className="">
-              Enter Bill
-            </Link>
+          <div className="home-title-container">
+            <div className="text-[1.5rem] italic">Welcome to</div>
+            <div className="uppercase text-[4rem] font-extrabold tracking-wide mt-[-0.5rem]">
+              Split.it
+            </div>
+            <span className="w-[30rem] text-[0.9rem] text-justify lg:text-[1.25rem]">
+              "<span className="underline font-semibold">Seamlessly</span>{' '}
+              divide your bills according to individual choices or equally,
+              guaranteeing everyone pays their fair share with ease."
+            </span>
+          </div>
+          <div className="flex justify-center my-[6rem]">
+            <div className="w-[15rem] text-center bg-neutral-800 hover:bg-neutral-700 text-white font-bold py-3 px-4 rounded-lg">
+              <Link to="/bill" className="">
+                Enter Bill
+              </Link>
+            </div>
           </div>
         </div>
       </div>
