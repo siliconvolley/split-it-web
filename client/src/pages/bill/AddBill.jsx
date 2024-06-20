33d89bd90525dea9657fe55b271ff8b94afedfe3
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-06-21 01:04:17 +0530

    refactored and finish the design of the layout

--- client/src/pages/bill/AddBill.jsx
+++ client/src/pages/bill/AddBill.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import BackArrow from '../../assets/back_arrow.svg';
+import { ChevronLeft } from 'lucide-react';
 
 function AddBill() {
   const [title, setTitle] = useState('');
@@ -29,118 +29,122 @@ function AddBill() {
   };
 
   const totalAmount = items.reduce(
-    (acc, item) => acc + item.price,
+    (acc, item) => Number(acc) + Number(item.quantity) * Number(item.price),
     0
   );
 
   return (
     <>
-      <div className="flex justify-center min-h-screen w-full">
-        <div className="max-w-[40rem] w-full m-5">
-          <div className="m-5">
-            <div className="flex items-center w-full">
-              <div className="absolute flex-shrink-0">
-                <Link to="/">
-                  <img src={BackArrow} alt="Back Arrow" className="w-8 h-8" />
-                </Link>
-              </div>
-              <span className="text-2xl font-bold flex-grow text-center">
-                Add Bill
-              </span>
-            </div>
-            <div className="my-8 mx-2 display-bill flex justify-center">
-              <div className="w-[28rem]  py-4 px-4 bg-neutral-200 rounded-lg flex flex-col justify-center">
-                <input
-                  type="text"
-                  placeholder="Enter Bill Title"
-                  value={title}
-                  onChange={e => setTitle(e.target.value)}
-                  className="h-[2rem] font-semibold text-center bg-inherit"
-                />
+      <div className="w-full flex justify-center">
+        <div className="max-w-[35rem] relative">
+          <nav className="flex mx-4 my-8">
+            <Link to="/" className="absolute">
+              <ChevronLeft color="#262626" size="32" />
+            </Link>
+            <span className="text-2xl font-bold flex-grow text-center">
+              Add Bill
+            </span>
+          </nav>
 
-                <span className="text-center">
-                  {new Date().toLocaleString()}
-                </span>
+          <main className="flex justify-center">
+            <div className="max-w-[35rem] w-full">
+              <div className="my-8 mx-2 display-bill flex justify-center">
+                <div className="w-[28rem] p-4 bg-neutral-50 shadow hover:shadow-lg transition-shadow duration-300 ease-in-out rounded-lg flex flex-col justify-center">
+                  <input
+                    type="text"
+                    placeholder="Enter Bill Title"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    className="h-[2rem] text-[1.25rem] font-extrabold text-center bg-inherit"
+                  />
 
-                <div className="flex justify-between">
-                  <span className="font-bold">Total: </span>
-                  <span>Rs. {totalAmount.toFixed(2)}</span>
-                </div>
+                  <span className="text-center">
+                    {new Date().toLocaleString()}
+                  </span>
 
-                <div className="grid my-4">
-                  {items.map(item => (
+                  <div className="flex justify-between">
+                    <span className="font-bold">Total: </span>
+                    <span>Rs. {totalAmount.toFixed(2)}</span>
+                  </div>
+
+                  <div className="grid my-4">
+                    {items.map(item => (
+                      <div
+                        key={item.serialNumber}
+                        className="grid grid-flow-col grid-cols-4 gap-0 bg-inherit [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                        style={{ gridTemplateColumns: '10% 50% 20% 20%' }}
+                      >
+                        <div className="">{item.serialNumber}.</div>
+                        <div className="">{item.name}</div>
+                        <div className="text-end">x{item.quantity} </div>
+                        <div className="text-end">{item.price.toFixed(2)}</div>
+                      </div>
+                    ))}
                     <div
-                      key={item.serialNumber}
-                      className="grid grid-flow-col grid-cols-4 gap-0 bg-inherit [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-                      style={{ gridTemplateColumns: '10% 50% 20% 20%' }}
+                      className="grid grid-flow-col grid-cols-3 gap-0 pt-1 bg-inherit [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                      style={{ gridTemplateColumns: '60% 20% 20%' }}
                     >
-                      <div className="">{item.serialNumber}.</div>
-                      <div className="">{item.name}</div>
-                      <div className="text-end">x{item.quantity} </div>
-                      <div className="text-end">{item.price}</div>
+                      <input
+                        ref={itemNameInputRef}
+                        type="text"
+                        placeholder="Item Name"
+                        value={itemName}
+                        className="bg-inherit px-2 py-1 w-full"
+                        onChange={e => setItemName(e.target.value)}
+                        onKeyDown={e => {
+                          if (e.key === 'Enter') {
+                            quantityInputRef.current.focus();
+                          }
+                        }}
+                      />
+                      <input
+                        ref={quantityInputRef}
+                        type="number"
+                        placeholder="Quantity"
+                        value={itemQuantity}
+                        className="bg-inherit w-full px-2 py-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-end"
+                        onChange={e => setItemQuantity(e.target.value)}
+                        onKeyDown={e => {
+                          if (e.key === 'Enter') {
+                            priceInputRef.current.focus();
+                          }
+                        }}
+                      />
+                      <input
+                        ref={priceInputRef}
+                        type="number"
+                        placeholder="Price"
+                        value={itemPrice}
+                        className="bg-inherit w-full px-2 py-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-end"
+                        onChange={e => setItemPrice(e.target.value)}
+                        onKeyDown={e => {
+                          if (e.key === 'Enter') {
+                            addItem();
+                          }
+                        }}
+                      />
                     </div>
-                  ))}
-                  <div
-                    className="grid grid-flow-col grid-cols-3 gap-0 bg-inherit pt-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-                    style={{ gridTemplateColumns: '60% 20% 20%' }}
-                  >
-                    <input
-                      ref={itemNameInputRef}
-                      type="text"
-                      placeholder="Item Name"
-                      value={itemName}
-                      className="bg-inherit px-2 py-1 w-full"
-                      onChange={e => setItemName(e.target.value)}
-                      onKeyDown={e => {
-                        if (e.key === 'Enter') {
-                          quantityInputRef.current.focus();
-                        }
-                      }}
-                    />
-                    <input
-                      ref={quantityInputRef}
-                      type="number"
-                      placeholder="Quantity"
-                      value={itemQuantity}
-                      className="bg-inherit w-full px-2 py-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-end"
-                      onChange={e => setItemQuantity(e.target.value)}
-                      onKeyDown={e => {
-                        if (e.key === 'Enter') {
-                          priceInputRef.current.focus();
-                        }
-                      }}
-                    />
-                    <input
-                      ref={priceInputRef}
-                      type="number"
-                      placeholder="Price"
-                      value={itemPrice}
-                      className="bg-inherit w-full px-2 py-1 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-end"
-                      onChange={e => setItemPrice(e.target.value)}
-                      onKeyDown={e => {
-                        if (e.key === 'Enter') {
-                          addItem();
-                        }
-                      }}
-                    />
+                  </div>
+                  <div>
+                    <button
+                      onClick={addItem}
+                      className="text-center hover:bg-neutral-300 text-neutral-800 py-1 px-2 rounded-lg"
+                    >
+                      + Add Item
+                    </button>
                   </div>
                 </div>
-                <div>
-                  <button
-                    onClick={addItem}
-                    className="text-center hover:bg-neutral-300 text-neutral-800 py-1 px-2 rounded-lg"
-                  >
-                    + Add Item
-                  </button>
-                </div>
+              </div>
+              <div className="flex justify-center my-[6rem]">
+                <Link
+                  to="/bill/split"
+                  className="w-[15rem] text-center bg-neutral-800 hover:bg-neutral-700 transition-colors duration-150 ease-in-out text-white font-bold py-3 px-4 rounded-lg"
+                >
+                  Add Bill
+                </Link>
               </div>
             </div>
-            <div className="flex justify-center my-[6rem]">
-              <div className="w-[15rem] text-center bg-neutral-800 hover:bg-neutral-700 text-white font-bold py-3 px-4 rounded-lg">
-                <Link>Add Bill</Link>
-              </div>
-            </div>
-          </div>
+          </main>
         </div>
       </div>
     </>
