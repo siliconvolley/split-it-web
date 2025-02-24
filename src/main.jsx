6db32cd9eb653445fdeab7d5203fcb47a95c3359
siliconvolley <rishabhlaxmikanth@gmail.com>
2025-02-25 00:49:08 +0530

    implemented routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+import HomePage from '@pages/homepage/HomePage';
+import AddBill from '@pages/bill/AddBill';
+import AddFriends from '@pages/addfriends/AddFriends';
+import Shares from '@pages/shares/Shares';
+import UploadImage from '@pages/ocr/UploadImage';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage/>
+  },
+  {
+    path: '/bill',
+    element: <AddBill/>
+  },
+  {
+    path: '/bill/friends',
+    element: <AddFriends/>
+  },
+  {
+    path: '/bill/shares',
+    element: <Shares/>
+  },
+  {
+    path: '/ocr',
+    element: <UploadImage/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
