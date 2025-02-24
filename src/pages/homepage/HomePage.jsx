999469f84f1b829626b229e815f4aab86e160366
siliconvolley <rishabhlaxmikanth@gmail.com>
2025-02-25 01:16:19 +0530

    removed unnecessary react imports

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import HomeIllustration from '@public/home_illustration.png';
 
--- src/pages/ocr/UploadImage.jsx
+++ src/pages/ocr/UploadImage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { createWorker } from 'tesseract.js';
 
 export default function UploadImage() {
