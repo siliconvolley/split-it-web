2144f32b2ac7270284ec0662de6e3ba214decf4f
siliconvolley <rishabhlaxmikanth@gmail.com>
2025-02-25 01:15:46 +0530

    refactored to routes service

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-
-import HomePage from '@pages/homepage/HomePage';
-import AddBill from '@pages/bill/AddBill';
-import AddFriends from '@pages/addfriends/AddFriends';
-import Shares from '@pages/shares/Shares';
-import UploadImage from '@pages/ocr/UploadImage';
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage/>
-  },
-  {
-    path: '/bill',
-    element: <AddBill/>
-  },
-  {
-    path: '/bill/friends',
-    element: <AddFriends/>
-  },
-  {
-    path: '/bill/shares',
-    element: <Shares/>
-  },
-  {
-    path: '/ocr',
-    element: <UploadImage/>
-  }
-]);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppRouter from '@routes/AppRouter';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AppRouter/>
   </React.StrictMode>,
 )
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import HomePage from '@pages/homepage/HomePage';
+import AddBill from '@pages/bill/AddBill';
+import AddFriends from '@pages/addfriends/AddFriends';
+import Shares from '@pages/shares/Shares';
+import UploadImage from '@pages/ocr/UploadImage';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage/>
+  },
+  {
+    path: '/bill',
+    element: <AddBill/>
+  },
+  {
+    path: '/bill/friends',
+    element: <AddFriends/>
+  },
+  {
+    path: '/bill/shares',
+    element: <Shares/>
+  },
+  {
+    path: '/ocr',
+    element: <UploadImage/>
+  }
+]);
+
+export default function AppRouter() {
+    return(<RouterProvider router={router} />);
+}
