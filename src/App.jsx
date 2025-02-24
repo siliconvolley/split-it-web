c9dc8b6dd9d78875afaf22c65730e5f31b7bfb4a
siliconvolley <rishabhlaxmikanth@gmail.com>
2025-02-24 19:01:41 +0530

    added route to /ocr

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/HomePage';
 import AddBill from './pages/bill/AddBill';
 import AddFriends from './pages/addfriends/AddFriends';
 import Shares from './pages/shares/Shares';
+import UploadImage from './pages/ocr/UploadImage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/bill" element={<AddBill />} />
         <Route path="/bill/friends" element={<AddFriends />} />
         <Route path="/bill/shares" element={<Shares/>} />
+        <Route path="/ocr" element={<UploadImage/>} />
       </Routes>
     </Router>
   );
