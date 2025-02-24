43a0c81e4ba6721bbb5e4ff3e1ca9fd71d6242f9
siliconvolley <rishabhlaxmikanth@gmail.com>
2025-02-25 00:50:00 +0530

    added import alias

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@public': '/src/public',
+      '@utils': '/src/utils'
+    },
+  },
   plugins: [react()],
   server: {
     host: true,
