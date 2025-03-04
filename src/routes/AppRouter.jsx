8cef0f8f41f6ac7411f3cc834367270a7f41474e
siliconvolley <rishabhlaxmikanth@gmail.com>
2025-03-04 22:39:11 +0530

    fixed imports

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,11 +1,10 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import HomePage from '@pages/home/HomePage';
-import AddBill from '@pages/bill/AddBill';
-import AddFriends from '@pages/friends/AddFriends';
-import Shares from '@pages/shares/Shares';
-import UploadImage from '@pages/ocr/UploadImage';
-
+import HomePage from '@/pages/HomePage';
+import AddBill from '@/pages/AddBillPage';
+import AddFriends from '@/pages/AddFriendsPage';
+import Shares from '@/pages/DisplaySharesPage';
+import UploadImage from '@/pages/UploadImagePage';
 
 const router = createBrowserRouter([
   {
