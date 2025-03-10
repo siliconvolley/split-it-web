86f5b790b757573745d12126cb682dd97b8e88f3
siliconvolley <rishabhlaxmikanth@gmail.com>
2025-03-10 19:13:31 +0530

    added page layouts

---
+++ src/pages/PageLayout.jsx
@@ -0,0 +1,7 @@
+export default function PageLayout({ children }) {
+  return (
+    <div className="h-svh max-w-[40rem] flex flex-col justify-start mx-auto px-5">
+      {children}
+    </div>
+  );
+}
