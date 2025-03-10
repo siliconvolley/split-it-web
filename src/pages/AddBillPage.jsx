465593acac092cf66207a7e10c62bc8d9eec4d8f
siliconvolley <rishabhlaxmikanth@gmail.com>
2025-03-10 19:14:15 +0530

    added page layout

--- src/pages/AddBillPage.jsx
+++ src/pages/AddBillPage.jsx
@@ -1,18 +1,16 @@
+import PageLayout from './PageLayout';
 import Navbar from '@/components/Navbar';
-import AddBill from '@/components/AddBill';
+import BillEntryForm from '@/components/AddBill/BillEntryForm';
 import Button from '@/components/Button';
 
 export default function AddBillPage() {
-
   return (
-    <div className="h-svh max-w-[40rem] flex flex-col justify-start mx-auto px-5">
-      <Navbar path='/' title='Add Bill' />
-
-      <AddBill />
-
+    <PageLayout>
+      <Navbar path="/" title="Add Bill" />
+      <BillEntryForm />
       <Button path="/bill/friends" className="my-16">
         Add Bill
       </Button>
-    </div>
+    </PageLayout>
   );
 }
