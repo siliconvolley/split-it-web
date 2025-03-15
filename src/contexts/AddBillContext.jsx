2c3b7d64abc7323a57f4d86dc0af918887dbe21d
siliconvolley <rishabhlaxmikanth@gmail.com>
2025-03-15 18:07:06 +0530

    deleted/renamed

--- src/contexts/AddBillContext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-const AddBillContext = createContext();
-
-export const AddBillProvider = ({ children }) => {
-  return <AddBillContext.Provider>{children}</AddBillContext.Provider>;
-};
