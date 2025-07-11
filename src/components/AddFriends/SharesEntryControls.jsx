a595e8944da830d0036da2e48eba01ad7831fdfc
siliconvolley <rishabhlaxmikanth@gmail.com>
2025-07-12 00:38:25 +0530

    display bill items from localStorage

--- src/components/AddFriends/SharesEntryControls.jsx
+++ src/components/AddFriends/SharesEntryControls.jsx
@@ -1,12 +1,19 @@
 import { Utensils } from 'lucide-react';
 
 import Button from '@/components/Button';
-import { getBillTitle, getBillTimestamp, getTotalAmount } from '@/utils/BillStorage';
+import {
+  getBillTitle,
+  getBillTimestamp,
+  getTotalAmount,
+} from '@/utils/BillStorage';
+import { useAddFriends } from '@/hooks/useAddFriends';
 
 export default function SharesEntryControls() {
   const billTitle = getBillTitle();
   const timestamp = getBillTimestamp();
   const totalAmount = getTotalAmount();
+
+  const { checkIfFriendsExist } = useAddFriends();
 
   return (
     <section className="flexible-container bg-neutral-50 grid grid-row-2 gap-4 p-3 rounded-lg shadow hover:shadow-lg transition-shadow duration-300 ease-in-out sm:grid-flow-col">
@@ -28,17 +35,28 @@ export default function SharesEntryControls() {
         </div>
         <div className="text-right sm:text-left">
           <h2 className="font-semibold">Total:</h2>
-          <p className="text-lg font-bold text-nowrap">₹ {totalAmount.toFixed(2)}</p>
+          <p className="text-lg font-bold text-nowrap">
+            ₹ {totalAmount.toFixed(2)}
+          </p>
         </div>
       </div>
       <div
         id="buttons-wrapper"
         className="flex justify-between space-x-3 sm:flex-col sm:space-x-0 sm:space-y-4"
       >
-        <Button type="secondary" path="/bill/shares" className="w-full">
+        <Button
+          type="secondary"
+          path="/bill/shares"
+          className="w-full"
+          disabled={!checkIfFriendsExist()}
+        >
           Split Evenly
         </Button>
-        <Button path="/bill/shares" className="w-full">
+        <Button
+          path="/bill/shares"
+          className="w-full"
+          disabled={!checkIfFriendsExist()}
+        >
           Split
         </Button>
       </div>
--- src/contexts/AddFriendsContextProvider.jsx
+++ src/contexts/AddFriendsContextProvider.jsx
@@ -1,12 +1,43 @@
 import { useState } from 'react';
 import { AddFriendsContext } from '@/contexts/AllContexts';
-import { getBillItems } from '@/utils/BillStorage';
+import { getBillData, getBillItems } from '@/utils/BillStorage';
 
 export default function AddFriendsContextProvider({ children }) {
   const [addFriendPopover, setAddFriendPopover] = useState(false);
   const [addSharesPopover, setAddSharesPopover] = useState(false);
   const [items, setItems] = useState(getBillItems);
-  
+  const [friends, setFriends] = useState(getBillData().friends || []);
+  const [newFriend, setNewFriend] = useState({
+    name: '',
+    share: 0,
+  });
+
+  const addFriend = () => {
+    if (!newFriend.name || newFriend.share <= 0) return;
+
+    const friendToAdd = {
+      name: newFriend.name,
+      share: Number(newFriend.share),
+    };
+
+    setFriends( prevFriends => {
+      const updatedFriends = [...prevFriends, friendToAdd];
+      const billData = getBillData();
+      billData.friends = updatedFriends;
+      // Save the updated friends list to storage
+      localStorage.setItem('billData', JSON.stringify(billData));
+      return updatedFriends;
+    });
+    
+    setNewFriend({ name: '', share: 0 });
+  }
+
+  const checkIfFriendsExist = () => {
+    if (friends.length === 0) {
+      return false;
+    }
+    return true;
+  };
 
   const value = {
     addFriendPopover,
@@ -15,6 +46,7 @@ export default function AddFriendsContextProvider({ children }) {
     setAddSharesPopover,
     items,
     setItems,
+    checkIfFriendsExist,
   };
 
   return (
